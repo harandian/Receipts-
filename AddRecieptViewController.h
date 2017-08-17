4866e157f399a9d01b6ab4a244f06dc69c142d96
harandian <hharandian@gmail.com>
2017-08-17 13:20:21 -0700

    Data sources and initial UIs setup

---
+++ AddRecieptViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddRecieptViewController.h
+//  Receipts++
+//
+//  Created by Hirad on 2017-08-17.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AddRecieptViewController : ViewController
+
+@end
---
+++ AddRecieptViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddRecieptViewController.m
+//  Receipts++
+//
+//  Created by Hirad on 2017-08-17.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import "AddRecieptViewController.h"
+
+@interface AddRecieptViewController ()
+
+@end
+
+@implementation AddRecieptViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
